0cb2d3b665d93c40eccd03e30b61a597d2f4b4c1
Bhuvaneshwar-PH <160835317+Bhuvaneshwar-PH@users.noreply.github.com>
2024-06-21 17:51:04 +0530

    Update dockerLogin.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -2,3 +2,4 @@ def call() {
   withCredentials([usernamePassword(credentialsId: 'nexus-cred', passwordVariable: 'passwd', usernameVariable: 'username')]) {
   sh "docker login -u $username -p $passwd 172.17.0.3:5000"
 }
+}
